eb40c286ab54a44f4d6d462d556debc0ef0ac7cf
deepali-lulekar <98205192+deepali-lulekar@users.noreply.github.com>
2023-05-08 15:57:54 +0530

    Update entrypoint.sh

--- mongo-data/entrypoint.sh
+++ mongo-data/entrypoint.sh
@@ -10,12 +10,12 @@ if [[] $(mongo ${MONGO_DB_URI} --eval 'db.getMongo().getDBNames().indexOf("sampl
     exit 0
 fi
 
-wget https://raw.githubusercontent.com/ozlerhakan/mongodb-json-files/master/datasets/companies.json
-mongoimport --drop -c companies --uri ${MONGO_DB_URI} companies.json
-rm companies.json
+// wget https://raw.githubusercontent.com/ozlerhakan/mongodb-json-files/master/datasets/companies.json
+// mongoimport --drop -c companies --uri ${MONGO_DB_URI} companies.json
+// rm companies.json
 
-wget https://github.com/ozlerhakan/mongodb-json-files/raw/master/datasets/tweets.zip
-unzip tweets.zip
-mongorestore --drop --uri ${MONGO_DB_URI} --nsInclude=samples.tweets dump/twitter/tweets.bson
-rm -rf dump tweets.zip
-mongo ${MONGO_DB_URI} --eval 'db.tweets.updateMany({}, [{ "$set": { "created_at": { "$toDate": "$created_at" } }}]);'
+// wget https://github.com/ozlerhakan/mongodb-json-files/raw/master/datasets/tweets.zip
+// unzip tweets.zip
+// mongorestore --drop --uri ${MONGO_DB_URI} --nsInclude=samples.tweets dump/twitter/tweets.bson
+// rm -rf dump tweets.zip
+// mongo ${MONGO_DB_URI} --eval 'db.tweets.updateMany({}, [{ "$set": { "created_at": { "$toDate": "$created_at" } }}]);'
